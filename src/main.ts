d58c3133bf2c83d5357925da32b50a0d006fd414
Ashutosh Varma <ashutoshvarma11@live.com>
2020-03-22 23:49:21 +0530

    add initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,49 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as io from '@actions/io'
+import * as util from './util'
+import fs from 'fs'
+import JSZip, {JSZipObject} from 'jszip'
+import path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const ninjaVersion: string = core.getInput('version', {required: false})
+    const ninjaDest: string = core.getInput('dest') || 'ninja_binary'
+    const ninjaPlatform: string = util.getPlatform(core.getInput('platform'))
+    if (!ninjaPlatform) throw Error('Unsupported Platform')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const ninjaDwdUrl = `https://github.com/ninja-build/ninja/releases/download/v${ninjaVersion}/ninja-${ninjaPlatform}.zip`
 
-    core.setOutput('time', new Date().toTimeString())
+    let ninjaBinaryName = 'ninja'
+    if (util.IS_WIN) {
+      ninjaBinaryName = `${ninjaBinaryName}.exe`
+    }
+
+    // create folders
+    io.mkdirP(ninjaDest)
+
+    const ninjaFilepath = path.join(ninjaDest, ninjaBinaryName)
+
+    const buff = await util.downloadAsBuffer(ninjaDwdUrl)
+
+    const ninjaZip: JSZip = await JSZip.loadAsync(buff)
+    const ninjaBinary: JSZipObject = ninjaZip.files[ninjaBinaryName]
+    if (ninjaBinary) {
+      await ninjaBinary.async('nodebuffer').then(content => {
+        fs.writeFileSync(ninjaFilepath, content)
+      })
+    } else {
+      throw Error(`Could not found "${ninjaBinaryName}" in the downloaded file`)
+    }
+
+    // make binary executable
+    fs.chmodSync(ninjaFilepath, '755')
+
+    // add to path
+    core.addPath(ninjaDest)
+
   } catch (error) {
     core.setFailed(error.message)
   }
 }
-
 run()
---
+++ src/util.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import request from 'request'
+
+export const IS_WIN: boolean = process.platform === 'win32'
+export const IS_LINUX: boolean = process.platform === 'linux'
+export const IS_MAC: boolean = process.platform === 'darwin'
+
+export async function downloadAsBuffer(url: string): Promise<Buffer> {
+  return new Promise<Buffer>((resolve, reject) => {
+    console.log(`Downloading file ${url}`)
+    request.get({url, encoding: null}, (err, res, body) => {
+      if (err) {
+        reject(err)
+      }
+      console.log(`Recieved ${res.statusCode}`)
+      if (res.statusCode >= 400) {
+        reject(Error(`Recieved ${res.statusCode} from ${url}`))
+      }
+      console.log(`Download Complete.`)
+      resolve(body)
+    })
+  })
+}
+
+export function getPlatform(platform: string): string {
+  if (!platform) {
+    return IS_MAC ? 'mac' : IS_LINUX ? 'linux' : IS_WIN ? 'win' : ''
+  } else {
+    return ['mac', 'linux', 'win'].includes(platform) ? platform : ''
+  }
+}
